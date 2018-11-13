b324af9d8ba39ee2a7b1c543d5490131c3bc6227
Stefano Coretta <stefanoc.social@me.com>
2018-11-13 10:35:50 +0100

    add measured variables

--- data-raw/prepare-data.R
+++ data-raw/prepare-data.R
@@ -3,4 +3,13 @@ library(tidyverse)
 stimuli <- read_csv("data-raw/stimuli.csv")
 
 ita_egg <- read_csv("data-raw/measurements.csv", na = "--undefined--") %>%
-  full_join(y = stimuli)
+  full_join(y = stimuli) %>%
+  mutate(
+    v1_duration = (c2_ons - v1_ons) * 1000,
+    rel_voff = (c2_ons - c1_rel) * 1000,
+    sent_duration = sentence_off - sentence_ons,
+    speech_rate = 8 / sent_duration,
+    voice_duration = (voice_off - voice_ons) * 1000,
+    vot = (voice_ons - c1_rel) * 1000,
+    voi_clo = (voice_off - c2_ons) * 1000
+  )
