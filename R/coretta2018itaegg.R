95c70bc10cd9b567faadf0e946955405a6c9427b
Stefano Coretta <stefanoc.social@me.com>
2018-11-13 11:06:19 +0100

    prepare package

---
+++ R/coretta2018itaegg.R
@@ -0,0 +1,10 @@
+#' coretta2018itaegg: Data Package of Vowel duration, voicing duration, and
+#' vowel height: Acoustic and articulatory data from Italian (Coretta 2018)
+#'
+#' This package provides datasets containing acoustic measurements of Italian.
+#' The scope of the data is to investigate the relation between vowel height,
+#' vowel duration, and voicing duration.
+#'
+#' @docType package
+#' @name coretta2018itaegg
+NULL
