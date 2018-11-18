b07d50ac754cfb53cf6df61706370a616adba83e
Stefano Coretta <stefanoc.social@me.com>
2018-11-18 17:51:58 +0100

    add variables

--- R/data.R
+++ R/data.R
@@ -5,36 +5,39 @@
 #' @docType data
 #' @format A tibble with 3268 observations and 31 variables.
 #' \describe{
-#'   \item{\code{speaker}}{}
-#'   \item{\code{ipu}}{}
-#'   \item{\code{stimulus}}{}
-#'   \item{\code{sentence_ons}}{}
-#'   \item{\code{sentence_off}}{}
-#'   \item{\code{word_ons}}{}
-#'   \item{\code{word_off}}{}
-#'   \item{\code{v1_ons}}{}
-#'   \item{\code{c2_ons}}{}
-#'   \item{\code{v2_ons}}{}
-#'   \item{\code{voice_ons}}{}
-#'   \item{\code{voice_off}}{}
-#'   \item{\code{c1_rel}}{}
-#'   \item{\code{c2_rel}}{}
-#'   \item{\code{stimulus_id}}{}
-#'   \item{\code{sentence}}{}
-#'   \item{\code{word}}{}
-#'   \item{\code{c1}}{}
-#'   \item{\code{vowel}}{}
-#'   \item{\code{c2}}{}
+#'   \item{\code{speaker}}{speaker's ID}
+#'   \item{\code{ipu}}{IPU index}
+#'   \item{\code{stimulus}}{sentence stimulus}
+#'   \item{\code{sentence_ons}}{time of sentence onset in seconds}
+#'   \item{\code{sentence_off}}{time of sentence offset in seconds}
+#'   \item{\code{word_ons}}{time of word onset (= C1 onset) in seconds}
+#'   \item{\code{word_off}}{time of word offset (= V2 ofset) in seconds}
+#'   \item{\code{v1_ons}}{time of V1 onset (= C1 offset) in seconds}
+#'   \item{\code{c2_ons}}{time of C2 onset (= V1 offset) in seconds}
+#'   \item{\code{v2_ons}}{time V2 onset (= C2 offset) in seconds}
+#'   \item{\code{voice_ons}}{time of voice onset in seconds}
+#'   \item{\code{voice_off}}{time of voice onset in seconds}
+#'   \item{\code{c1_rel}}{time of C1 release in seconds}
+#'   \item{\code{c2_rel}}{time of C2 release in seconds}
+#'   \item{\code{stimulus_id}}{index of the stimulus sentence}
+#'   \item{\code{sentence}}{frame sentence}
+#'   \item{\code{word}}{target word}
+#'   \item{\code{c1}}{C1 (\code{p, t, k})}
+#'   \item{\code{vowel}}{V1 (\code{i, e, u, ɛ, a})}
+#'   \item{\code{c2}}{C2 (\code{p, t, k})}
 #'   \item{\code{backness}}{}
 #'   \item{\code{height}}{}
-#'   \item{\code{c1_place}}{}
-#'   \item{\code{c2_place}}{}
-#'   \item{\code{v1_duration}}{}
-#'   \item{\code{rel_voff}}{}
-#'   \item{\code{sent_duration}}{}
-#'   \item{\code{speech_rate}}{}
-#'   \item{\code{voice_duration}}{}
-#'   \item{\code{vot}}{}
-#'   \item{\code{voi_clo}}{}
+#'   \item{\code{c1_place}}{C1 place of articulation}
+#'   \item{\code{c2_place}}{C2 place of articulation}
+#'   \item{\code{v1_duration}}{duration of V1 in milliseconds}
+#'   \item{\code{c2_clos_duration}}{duration of C1 closure in milliseconds}
+#'   \item{\code{rel_voff}}{duration of C1 release to V1 offset in milliseconds}
+#'   \item{\code{sent_duration}}{sentence duration in seconds}
+#'   \item{\code{speech_rate}}{speech rate as number of syllables per second (\code{= 8 / sentece_duration})}
+#'   \item{\code{speech_rate_c}}{centered speech rate (\code{= speech_rate - mean(speech_rate)})}
+#'   \item{\code{voice_duration}}{duration of the voiced interval of V1 in milliseconds}
+#'   \item{\code{vot}}{duration of Voice Onset Time in milliseconds}
+#'   \item{\code{voi_clo}}{duration of voicing during C2 closure in milliseconds}
+#'   \item{\code{rel_rel}}{duration of C1 release to C2 release}
 #' }
 "ita_egg"
--- data-raw/prepare-data.R
+++ data-raw/prepare-data.R
@@ -7,12 +7,19 @@ ita_egg <- read_csv("data-raw/measurements.csv", na = "--undefined--") %>%
   full_join(y = stimuli) %>%
   mutate(
     v1_duration = (c2_ons - v1_ons) * 1000,
+    c2_clos_duration = (c2_rel - c2_ons) * 1000,
     rel_voff = (c2_ons - c1_rel) * 1000,
     sent_duration = sentence_off - sentence_ons,
     speech_rate = 8 / sent_duration,
+    speech_rate_c = speech_rate - mean(speech_rate),
     voice_duration = (voice_off - voice_ons) * 1000,
     vot = (voice_ons - c1_rel) * 1000,
-    voi_clo = (voice_off - c2_ons) * 1000
+    voi_clo = (voice_off - c2_ons) * 1000,
+    rel_rel = (c2_rel - c1_rel) * 1000,
+    vowel = factor(vowel, levels = c("i", "e", "a", "o", "u")),
+    height = factor(height, levels = c("low", "mid-low", "mid-high", "high")),
+    c1_place = factor(c1_place, levels = c("labial", "coronal", "velar")),
+    c2_place = factor(c2_place, levels = c("labial", "coronal", "velar"))
   )
 
 use_data(ita_egg, overwrite = TRUE)
